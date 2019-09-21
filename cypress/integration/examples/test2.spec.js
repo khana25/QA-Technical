fa10376f8e54c781d41906ed37e987923df9e641
Ayub Khan <ayub_h@hotmail.com>
2019-09-21 21:06:20 +0100

    first commit

--- cypress/integration/examples/test2.spec.js
+++ cypress/integration/examples/test2.spec.js
@@ -2,18 +2,23 @@
 
 describe("BJSS Testing App 2", () => {
 
-    it("Login applicaConfirm that the blue, red, and green button ids", () => {
+    it("Confirm that the blue, red, and green button ids", () => {
 
         cy.visit("https://the-internet.herokuapp.com/");
         cy.contains('Dynamic Loading').click();
+
+        //Click the 'Element rendered after the fact'
 
         cy.contains('Example 2: Element rendered after the fact').click();
         cy.url().should("include", "/dynamic_loading/2");
 
         cy.get('button').click();
         
-       
+       // Wait for the loading bar fisappears and the final text appears after loading
         cy.wait(5000)
+
+        //verify that the expected text appears after loading and 
+        //clicking  the 'Element rendered after the fact'
 
         cy.get('#finish > h4').should('have.text', 'Hello World!')
 
--- cypress/integration/examples/test3.spec.js
+++ cypress/integration/examples/test3.spec.js
@@ -1,6 +1,6 @@
 describe("Test API from the Fake JSON server", () => {
 
-    it("Test POST functionality of JSON server", () => {
+    it("Test POST functionality", () => {
 
         cy.server();
 
@@ -15,6 +15,10 @@ describe("Test API from the Fake JSON server", () => {
             }
 
         }).then((res) => {
+
+            // verify that the user was made and has the same properties associated to those elements
+            expect(res.body).has.property("name", "test")
+            expect(res.body).has.property("salary", "123")
             expect(res.body).has.property("age", "23")
         })
 
